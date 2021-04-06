3fc9ceb29d446fe128071cb1bba83acd56823575
parksangwoo <b01056721617@gmail.com>
2021-04-06 14:53:19 +0900

    study

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let a = 1 + 2;
+let str = 'abc';
+let b = a + str;
+let c = {
+    apple: a,
+    banana: b
+};
+console.log(c.banana);
+let d = c.apple * 4;
+//# sourceMappingURL=index.js.map
