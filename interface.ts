6c0df67702062dd1c0f901250adca78f33d2d76e
parksangwoo <b01056721617@gmail.com>
2021-04-07 13:17:58 +0900

    interface.ts

---
+++ interface.ts
@@ -0,0 +1,17 @@
+function printLabel(labeledObj: { label: string}) {
+    console.log(labeledObj.label);
+}
+
+let myObj = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
+
+interface LabeledValue {
+    label: string;
+}
+
+function printLabel1(labeledObj: LabeledValue) {
+    console.log(labeledObj.label);
+}
+
+let myObj1 = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
